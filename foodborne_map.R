d8226312c872f47d3592b661da04b6fcbdc973ea
corynissen <corynissen@gmail.com>
2013-11-14 13:07:03 -0600

    Add ggmap and shapefile stuff... this is pretty cool

--- foodborne_map.R
+++ foodborne_map.R
@@ -3,16 +3,21 @@
 # This file creates a borderless map of Chicago using the openstreetmap tiles
 # and overlays the points from the submissions to foodborne chicago
 # a file called foodborne_chicago_submissions.png is created
+# Chicago city shapefiles downloaded from: http://goo.gl/8bHX1l
 #############################################################################
+
 library(ggplot2)
 library(OpenStreetMap)
 library(grid)
+library(ggmap)
+library(rgdal)
 source("read_and_geocode.R") # will take a couple minutes to geocode
 
 # keep only ones near chicago...
 df2 <- subset(df, lat > 41.62 & lat < 42.05 & lng > -87.96 & lng < -87.5 
               & !is.na(lat) & !is.na(lng))
 
+# graph using OpenStreetMap package
 merc <- projectMercator(df2$lat, df2$lng) #OSM uses mercator
 # give openmap the upper right and lower left corners you want for your map
 mp <- openmap(c(42.05, -87.96), c(41.62, -87.5), type="osm", minNumTiles=16)
@@ -28,3 +33,16 @@ ge <- subset(gt$layout, name == "panel")
 png("foodborne_chicago_submissions.png", width=1433, height=1800)
 grid.draw(gt[ge$t:ge$b, ge$l:ge$r])
 dev.off()
+
+# same graph using ggmap package
+p2 <- qmap("chicago") + geom_point(data=df2, aes(x=lng, y=lat)) +
+    coord_cartesian(xlim=c(-87.96, -87.5), ylim=c(41.62, 42.05))
+ggsave(plot=p2, "foodborne_chicago_submissions2.png", height=5, width=5)
+
+# now add shapefile info
+shapefile <- readOGR("chicago_city_shapefiles", "City_Boundary")
+shapefile.converted <- spTransform(shapefile, CRS("+proj=longlat +datum=WGS84"))
+p3 <- p2 + geom_polygon(aes(x = long, y = lat, group=group), alpha=.2, 
+                 data = shapefile.converted) + 
+    coord_cartesian(xlim=c(-87.96, -87.5), ylim=c(41.62, 42.05))
+ggsave(plot=p3, "foodborne_chicago_submissions3.png", height=5, width=5)
